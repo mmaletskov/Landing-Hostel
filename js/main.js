9e48c80cfc73c6c0bc598d03a402cf0b91f8cb0c
Михаил Малецков <97849486+mmaletskov@users.noreply.github.com>
2022-12-12 18:51:04 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.benefits_slider').slick({
+        // dots:true,
+        // fade:true, /* Чтобы картинки переключались плавно и без прокрутки. Эффект появления */
+        // autoplay:true, /* Автообновление картинок */
+        // autoplaySpeed:2000, /* Скорость обновления картинок  */
+        slidesToShow: 3,
+        slidesToScroll: 1,
+
+    })
+})
